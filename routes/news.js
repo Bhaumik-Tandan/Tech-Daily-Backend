f870553e1b9ead7119c58bed2932a1403f3d648d
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-16 20:14:15 +0530

    [add] get news api

--- routes/news.js
+++ routes/news.js
@@ -1,11 +1,27 @@
 import { Router } from 'express';
+import News from '../models/news.js';
 
 const router = Router();
 
-router.route('/').get((req, res) => {
-    res.json({ message: 'Hello World' });
+router.route('/').get(async (req, res) => {
+  try {
+    // Pagination parameters
+    const page = parseInt(req.query.page) || 1;  // Current page (default to 1)
+    const perPage = 50;  // Number of items per page
+
+    // Calculate the skip value to start from the right index
+    const skip = (page - 1) * perPage;
+
+    // Query the database to get the sorted and paginated news
+    const news = await News.find()
+      .sort({ createAt: -1 })  // Sort by createAt date in descending order (newest first)
+      .skip(skip)
+      .limit(perPage);
+
+    res.json(news);
+  } catch (err) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
-
-
 
 export default router;
