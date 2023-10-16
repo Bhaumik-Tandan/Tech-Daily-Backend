b5a6e119a730f87d86dd8d9151ce05b07910751a
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-16 21:45:01 +0530

    [edit] CRON DELETEION AT 2 DAYS BACK

--- cron/index.js
+++ cron/index.js
@@ -4,7 +4,8 @@ import clearLastDayNews from './clearLastDayNews.js';
 
 function cronInit() {
     cron.schedule('0 * * * *', saveNewsToDb);
-    cron.schedule('0 0 * * *', clearLastDayNews);
+    // cron.schedule('0 0 * * *', clearLastDayNews);
+    cron.schedule('0 0 */2 * *', clearLastDayNews);
 }
 
 export default cronInit;
