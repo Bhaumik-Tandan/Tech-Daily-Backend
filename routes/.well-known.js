12acd974e0a1a5519d90157bc2758de12da9bb55
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-17 16:29:22 +0530

    [ADD] welknow arrya:

--- routes/.well-known.js
+++ routes/.well-known.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.route('/assetlinks.json').get(async (req, res) => {
-   const json= {
+   const json= [{
         "relation": [
         "delegate_permission/common.handle_all_urls"
         ],
@@ -14,7 +14,7 @@ router.route('/assetlinks.json').get(async (req, res) => {
         "40:06:E5:2E:47:01:BD:E7:D1:4D:D6:22:CE:8F:0A:F1:33:A0:2A:72:F0:59:52:D7:80:C6:1A:EA:E1:5E:3F:AF"
         ]
         }
-    };
+    }];
     res.json(json);
 });
 
