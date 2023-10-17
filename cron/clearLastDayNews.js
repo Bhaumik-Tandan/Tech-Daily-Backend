91e0c777f7b38deedb232635743a6eb579b38b42
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-18 00:38:18 +0530

    [FIX] cron

--- cron/clearLastDayNews.js
+++ cron/clearLastDayNews.js
@@ -3,7 +3,7 @@ import News from '../models/news.js';
 async function clearLastDayNews() {
     console.log(Date(),triggered);
     const currentDateTime = new Date();
-    currentDateTime.setHours(currentDateTime.getHours() - 48);
+    currentDateTime.setHours(currentDateTime.getHours() - 24);
     const iso8601Time = currentDateTime.toISOString();
     await News.deleteMany({ createdAt: { $lte: iso8601Time } });
 }
