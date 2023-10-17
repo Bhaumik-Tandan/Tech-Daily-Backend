a46e53b1c7b5c642ad8ea8047f6c123eb60003d6
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-17 05:32:25 +0530

    [ADD] single article page

--- routes/news.js
+++ routes/news.js
@@ -3,6 +3,7 @@ import News from '../models/news.js';
 
 const router = Router();
 
+// Get all news articles with pagination
 router.route('/').get(async (req, res) => {
   try {
     // Pagination parameters
@@ -14,9 +15,25 @@ router.route('/').get(async (req, res) => {
 
     // Query the database to get the sorted and paginated news
     const news = await News.find()
-      .sort({ createAt: -1 })  // Sort by createAt date in descending order (newest first)
+      .sort({ createdAt: -1 })  // Sort by createdAt date in descending order (newest first)
       .skip(skip)
       .limit(perPage);
+
+    res.json(news);
+  } catch (err) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Get a single news article by ID
+router.route('/:id').get(async (req, res) => {
+  try {
+    const newsId = req.params.id;
+    const news = await News.findById(newsId);
+
+    if (!news) {
+      return res.status(404).json({ error: 'News article not found' });
+    }
 
     res.json(news);
   } catch (err) {
