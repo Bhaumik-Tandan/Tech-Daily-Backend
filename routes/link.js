bd711db7bc78d18f854606c9eb8c2aacf86c2e7e
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-17 20:23:36 +0530

    [FIX[ meta data deep link

--- routes/link.js
+++ routes/link.js
@@ -5,6 +5,10 @@ const router = Router();
 router.get('/', (req, res) => {
     const query = req.query;
   const html = `
+  <meta property="og:site_name" content="Tech Daily">
+<meta property="og:title" content="One Stop Solution For All Tech News" />
+<meta property="og:description" content="Download the App from the play store" />
+<meta property="og:image" itemprop="image" content="https://assets.thehansindia.com/h-upload/2021/05/25/1078280-tech.webp">
     <h1>Please Wait...</h1>
     <script>
         function openStore() {
