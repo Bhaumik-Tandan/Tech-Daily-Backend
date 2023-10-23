265ed0f88c5920798d3805797e12540c146d0587
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-24 01:14:16 +0530

    [ADD] removal time of old news to 12 hours

--- cron/clearLastDayNews.js
+++ cron/clearLastDayNews.js
@@ -3,7 +3,7 @@ import News from '../models/news.js';
 async function clearLastDayNews() {
     console.log(Date(),"triggered");
     const currentDateTime = new Date();
-    currentDateTime.setHours(currentDateTime.getHours() - 2);
+    currentDateTime.setHours(currentDateTime.getHours() - 12);
     const iso8601Time = currentDateTime.toISOString();
     const newsCount = await News.countDocuments();
     if (newsCount > 100) {
