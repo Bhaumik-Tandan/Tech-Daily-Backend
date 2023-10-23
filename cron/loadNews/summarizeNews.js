17745fa4e2ef6d8d763922e66cc9ee458bcb15d3
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-23 13:45:19 +0530

    [add] TEXT

--- cron/loadNews/summarizeNews.js
+++ cron/loadNews/summarizeNews.js
@@ -7,11 +7,27 @@ function summarizeNews(title, content) {
                 console.log("Something went wrong man!");
                 reject(err);
             } else {
-                // Split the summary by newline and remove the first line (title)
-                const lines = summary.split('\n');
+                // Replace single line breaks with double line breaks
+                summary = summary.replace(/\n/g, '\n\n');
+
+                // Split the summary by double line breaks and remove the first line (title)
+                const lines = summary.split('\n\n');
                 lines.shift(); // Remove the first line (title)
-                const newSummary = lines.join('\n');
-                resolve(newSummary);
+
+                // Combine sentences until the summary has 60 or fewer words
+                let wordCount = 0;
+                const newSummary = [];
+                for (const line of lines) {
+                    const words = line.split(/\s+/);
+                    if (wordCount + words.length <= 60) {
+                        newSummary.push(line);
+                        wordCount += words.length;
+                    } else {
+                        break;
+                    }
+                }
+
+                resolve(newSummary.join('\n\n'));
             }
         });
     });
